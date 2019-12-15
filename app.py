c6cad7555fd4a441d23c3f561166e4beaae74722
aleks <delov.alesha@yandex.ru>
2019-12-15 22:50:17 +0300

    add fav

--- app.py
+++ app.py
@@ -25,6 +25,16 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route("/name/<name>")
 def get_book_name(name):
     return "name : {}".format(name)
