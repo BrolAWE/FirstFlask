31d04959db2626fc2be1cd8a1074f4ef68d2f75d
aleks <delov.alesha@yandex.ru>
2019-12-17 00:01:25 +0300

    fix database

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import os
 from datetime import datetime
 
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -22,21 +21,31 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config.from_object(config('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+from models import *
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-from models import Book
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-    form.name.data = ''
-    return render_template('index.html', form=form, name=name,
-                           current_time=datetime.utcnow())
+        user = User.query.ﬁlter_by(username=form.name.data).ﬁrst()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            db.session.commit()
+        else:
+            session['known'] = True
+            db.session.commit()
+        session['name'] = form.name.data
+        db.session.commit()
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -52,74 +61,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
-
-@app.route("/name/<name>")
-def get_book_name(name):
-    return "name : {}".format(name)
-
-
-@app.route("/details")
-def get_book_details():
-    author = request.args.get('author')
-    published = request.args.get('published')
-    return "Author : {}, Published: {}".format(author, published)
-
-
-@app.route("/add")
-def add_book():
-    name = request.args.get('name')
-    author = request.args.get('author')
-    published = request.args.get('published')
-    try:
-        book = Book(
-            name=name,
-            author=author,
-            published=published
-        )
-        db.session.add(book)
-        db.session.commit()
-        return "Book added. book id={}".format(book.id)
-    except Exception as e:
-        return (str(e))
-
-
-@app.route("/getall")
-def get_all():
-    try:
-        books = Book.query.all()
-        return jsonify([e.serialize() for e in books])
-    except Exception as e:
-        return (str(e))
-
-
-@app.route("/get/<id_>")
-def get_by_id(id_):
-    try:
-        book = Book.query.filter_by(id=id_).first()
-        return jsonify(book.serialize())
-    except Exception as e:
-        return (str(e))
-
-
-@app.route("/add/form", methods=['GET', 'POST'])
-def add_book_form():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        author = request.form.get('author')
-        published = request.form.get('published')
-        try:
-            book = Book(
-                name=name,
-                author=author,
-                published=published
-            )
-            db.session.add(book)
-            db.session.commit()
-            return "Book added. book id={}".format(book.id)
-        except Exception as e:
-            return (str(e))
-    return render_template("getdata.html")
 
 
 if __name__ == '__main__':
