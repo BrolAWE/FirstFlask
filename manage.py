b340b88e18b18ba71b09022bf8bd09eae8418879
aleks <delov.alesha@yandex.ru>
2019-12-17 19:11:18 +0300

    fix

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from werkzeug.middleware.profiler import ProfilerMiddleware
 
 from app import create_app, db
+from app.models import User, Role
 
 app = create_app("default")
 manager = Manager(app)
@@ -18,3 +20,11 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+
+
+@manager.command
+def profile(length=25, profile_dir=None):
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+
+    app.run()
