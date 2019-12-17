2805a3ac276eb90c5ba6e4d91f727ff22f3e9b22
aleks <delov.alesha@yandex.ru>
2019-12-17 12:15:51 +0300

    fix

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from werkzeug.middleware.profiler import ProfilerMiddleware
 
 from app.main.views import app, db
 
@@ -10,3 +11,9 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+
+@manager.command
+def profile(length=25, profile_dir=None):
+    """Запускает приложение в режиме профилирования кода."""
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length], profile_dir=profile_dir)
+    app.run()
