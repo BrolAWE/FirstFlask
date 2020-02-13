982ac5048bf63cd7a06befa0bdcd2151badd9646
Alexey <aleksejdelov@gmail.com>
2020-02-13 16:33:24 +0300

    login

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,21 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 from config import configg
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
 login_manager = LoginManager()
-login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 
@@ -19,13 +25,17 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(configg[config_name])
     configg[config_name].init_app(app)
+
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    from app.main import main as main_blueprint
+
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_preﬁx='/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     return app
