84d42c8c536bfae4c052cac9d05efd1e29a5afa6
Alexey <aleksejdelov@gmail.com>
2020-02-14 10:14:32 +0300

    edit_profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,102 +1,64 @@
-from datetime import datetime
-
-import math
-from flask import render_template, session, redirect, url_for, flash, request
-from flask_login import login_required
-
-from app.email import send_email
+from flask import render_template, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import EditProfileForm, EditProfileAdminForm
 from .. import db
-from ..models import User
+from ..models import Role, User
+from ..decorators import admin_required
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.ﬁlter_by(username=form.name.data).ﬁrst()
-        if user is None:
-            flash('Новый пользователь!')
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if True:
-                send_email("aleksejdelov@gmail.com", 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False), current_time=datetime.utcnow())
+    return render_template('index.html')
 
 
-@main.route('/secret')
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
-def secret():
-    return 'Only authenticated users are allowed!'
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
 
 
-@main.route('/calc1', methods=['GET', 'POST'])
-def calc1():
-    return render_template('calc1.html')
-
-
-@main.route('/calc', methods=['GET', 'POST'])
-def calc():
-    size = request.args.get('siz')
-    return render_template('calc.html', ind=int(size))
-
-
-def shubic(n, k):
-    return((math.factorial(k-1)*math.factorial(n-k))/math.factorial(n))
-
-
-def subsets(S):
-    sets = []
-    len_S = len(S)
-    for i in range(1 << len_S):
-        subset = [S[bit] for bit in range(len_S) if i & (1 << bit)]
-        sets.append(subset)
-    return sets
-
-
-@main.route('/otv', methods=['GET', 'POST'])
-def otv():
-    S = []
-    size = request.args.get('siz')
-    q = 5
-    for i in range(int(size)):
-        zn=request.args.get(str(i)).strip()
-        S.append(["w" + str(i), int(zn)])
-    print(S)
-    coal = []
-    res = []
-    for m in subsets(S):
-        coal.append(m)
-    del coal[0]
-    for i in range(len(coal)):
-        tmp = 0
-        for j in coal[i]:
-            tmp += j[1]
-        if tmp >= q:
-            res.append(coal[i])
-    shu = [0] * len(S)
-    for i in range(len(S)):
-        for j in range(len(res)):
-            for k in range(len(res[j])):
-                if (res[j][k][0] == S[i][0]):
-                    tmp_res = res[j].copy()
-                    del tmp_res[k]
-                    tmp = 0
-                    for kk in tmp_res:
-                        tmp += kk[1]
-                    if tmp < q:
-                        n = len(S)
-                        ak = len(res[j])
-                        shu[i] += (shubic(n, ak))
-    print(shu)
-    return render_template('otv.html', otv=shu)
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
